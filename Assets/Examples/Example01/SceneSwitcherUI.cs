0adbefde126726eef760d5843099874d1b05ddaf
nnakata <nakata@team-lab.com>
2014-04-16 19:04:05 +0900

    no message

---
+++ Assets/Examples/Example01/SceneSwitcherUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcherUI : MonoBehaviour {
+	public string title;
+	public int width = 100;
+
+	void OnGUI() {
+		GUILayout.BeginVertical(GUILayout.Width(width));
+
+		GUILayout.Label(title);
+		if (GUILayout.Button("Next")) {
+			Application.LoadLevel((Application.loadedLevel + 1) % Application.levelCount);
+			return;
+		}
+
+		GUILayout.EndVertical();
+	}
+}
