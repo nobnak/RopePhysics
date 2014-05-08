b74ee07d2b96f3976eed167b2ac49c42abb0370c
nnakata <nakata@team-lab.com>
2014-05-08 19:04:17 +0900

    no message

---
+++ Assets/RopePhysics/RigidbodyLink.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyLink : MonoBehaviour {
+	public PointMass link;
+	public float k;
+
+	void FixedUpdate() {
+		var toLink = link.transform.position - rigidbody.position;
+		var f = k * toLink;
+		link.AddForce(-f);
+		rigidbody.AddForce(f);
+	}
+}
