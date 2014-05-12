facbef3f2cc31f5226911b6196f03c38b38a1d7f
nnakata <nakata@team-lab.com>
2014-05-12 11:13:02 +0900

    no message

---
+++ Assets/Examples/Example01/Canon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canon : MonoBehaviour {
+	public Transform bullet;
+	public Transform destination;
+	public float duration;
+
+	private GravitationalCurve _curve;
+
+	void Update () {
+		if (_curve == null && Input.GetMouseButtonDown(0)) {
+			StartCoroutine(Shoot());
+		}
+	}
+
+	IEnumerator Shoot() {
+		_curve = new GravitationalCurve(transform.position, destination.position, duration, Physics.gravity);
+		foreach (var p in _curve.Go()) {
+			bullet.position = p;
+			yield return null;
+		}
+		_curve = null;
+	}
+}
---
+++ Assets/RopePhysics/Geometry/GravitationalCurve.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GravitationalCurve {
+	private Vector3 _x0;
+	private Vector3 _xt;
+	private Vector3 _g;
+	private float _duration;
+
+	private Vector3 _v;
+
+	public GravitationalCurve(Vector3 x0, Vector3 xt, float duration, Vector3 g) {
+		this._x0 = x0;
+		this._xt = xt;
+		this._g = g;
+		this._duration = duration;
+		this._v = (1f / duration) * (xt - x0) - (0.5f * duration) * g;
+	}
+
+	public IEnumerable<Vector3> Go() {
+		var startTime = Time.timeSinceLevelLoad;
+		var t = 0f;
+		while (t < _duration) {
+			yield return _x0 + t * _v + (0.5f * t * t) * _g;
+			t = (Time.timeSinceLevelLoad - startTime);
+		}
+		yield return _xt;
+	}
+}
